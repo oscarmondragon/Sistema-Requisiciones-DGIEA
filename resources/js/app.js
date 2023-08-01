4040caf4020972cea6c589ce510a7d0e5d6b0cc3
Mercedes2512 <69169206+Mercedes2512@users.noreply.github.com>
2023-08-01 14:43:06 -0600

    cambio de color, adicion de icono y color tabla adqui
    Se cambio el color del pie de pagina y del encabezado de sección, así como algunas características de la tabla

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,3 +5,13 @@ import Alpine from 'alpinejs';
 window.Alpine = Alpine;
 
 Alpine.start();
+
+/*Esta seccion es para aparecer/desaparecer el menu responsivo*/
+const boton = document.querySelector('#btn_menu');
+const menu = document.querySelector('#menu');
+
+boton.addEventListener('click', () =>{
+    console.log('me diste click')
+    menu.classList.toggle('hidden')
+
+} )
