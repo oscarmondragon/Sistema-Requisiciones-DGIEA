a31fc3f556f5016e4002d54098a88ac17bb07592
Oscar Mondragón Alcántara <oscarmondragon100@gmail.com>
2024-02-08 09:36:17 -0600

    Ruta para documentos descargables desde cvu - solucion error variable solicitudes
    Aun no funciona, solo esta creado el componente
    Se resolvió un error de una variable en solicitudes

---
+++ resources/views/livewire/documentos-descargables.blade.php
@@ -0,0 +1,16 @@
+<div class="py-6">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+    @include('layouts.header-cvu', ['accion' => 4])
+    <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">            <div class="p-6 text-gray-900">
+                <div>
+                    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                        <h3>Documentos descargables </h3>
+                        <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                           
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
